bfe0398237c7b7dc4f88d1f82cdadb1aa9ecaae2
Rick Wierenga <rick_wierenga@icloud.com>
2023-02-15 12:22:00 +0100

    version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='opentrons http api client',
-    version="0.1.1",
+    version="0.1.2",
     python_requires=">=3.7",
     packages=find_packages(exclude="testing"),
     description='A Python library for interacting with the Opentrons HTTP API',
