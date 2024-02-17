a3befdcb849ba5f76eca60285bce3672d361f360
Rick Wierenga <rick_wierenga@icloud.com>
2024-02-17 13:37:50 -0500

    add home

--- ot_api/health.py
+++ ot_api/health.py
@@ -1,6 +1,10 @@
 """ Somewhat nicer wrapper around ot_api.runs for health related things """
 
-from ot_api.requestor import get as rget
+from ot_api.requestor import get as rget, post
 
 def get():
   return rget("/health")
+
+def home():
+  return post("/robot/home", data={"target": "robot"})
+
