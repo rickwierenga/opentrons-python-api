2ee726c7d69583104b15612e9a7c50c09a39fee4
lennijusten <lenni@securebio.org>
2024-04-30 15:40:08 -0400

    update thermocycler_run_profile

--- ot_api/modules.py
+++ ot_api/modules.py
@@ -63,13 +63,13 @@ def thermocycler_deactivate_block(module_id: str, run_id: str = None):
     {"moduleId": module_id}, intent="setup", run_id=run_id)
 
 @command
-def thermocycler_deactivate_lid(module_id: str, run_id: str = None):
+def thermocycler_deactivate_lid(profile: list, module_id: str, run_id: str = None):
   """ Deactivate thermocycler lid """
   return ot_api.runs.enqueue_command("thermocycler/deactivateLid",
-    {"moduleId": module_id}, intent="setup", run_id=run_id)
+    {"profile": profile, "moduleId": module_id}, intent="setup", run_id=run_id)
 
 @command
-def thermocycler_run_profile(module_id: str, run_id: str = None):
+def thermocycler_run_profile(profile: list, block_max_volume: float, module_id: str, run_id: str = None):
   """ Execute thermocycler profile run """
   return ot_api.runs.enqueue_command("thermocycler/runProfile",
-    {"moduleId": module_id}, intent="setup", run_id=run_id)
+    {"profile": profile, "blockMaxVolumeUl": block_max_volume,"moduleId": module_id}, intent="setup", run_id=run_id)
